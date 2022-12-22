5e0b6b6e7b7f5b90594263c95609d04c58a6fe17
Yugo Takeshita @Lance <takeshita.yugo@lance-digital.com>
2022-12-22 18:08:26 +0900

    職業に対応
    職業に対応

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ $(function () {
         var name = $('input[name="name"]').val();
         var date = $('input[name="date"]:checked').val();
         var area = $('input[name="area"]').val();
+        var work = $('input[name="work"]').val();
 
-        var msg = `氏名：${name}\n生年月日：${date}\nお住まいの地域：${area}`;
+        var msg = `氏名：${name}\n生年月日：${date}\nお住まいの地域：${area}\n職業：${work}`;
         sendText(msg);
 
         return false;

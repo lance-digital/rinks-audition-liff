b715310a7301af0c85082eac996f682b4375bb00
Yugo@Lance <takeshita.yugo@lance-digital.com>
2022-12-23 00:06:05 +0900

    変数格納を修正
    変数格納を修正

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ $(function () {
         //氏名
         var name = $('input[name="name"]').val();
         //生年月日
-        var year = $('input[name="year"]').val();
-        var month = $('input[name="month"]').val();
-        var day = $('input[name="day"]').val();
+        var year = $('#year').val();
+        var month = $('#month').val();
+        var day = $('#day').val();
         //住んでいる地域
-        var area = $('input[name="area"]').val();
+        var area = $('#area').val();
         //職業
-        var work = $('input[name="work"]').val();
+        var work = $('#work').val();
 
         var msg = `氏名：${name}
             \n生年月日：${year}年${month}月${day}日

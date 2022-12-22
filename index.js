e06ba742ead2fb9bd370a3c434287c5042eaae61
Yugo@Lance <takeshita.yugo@lance-digital.com>
2022-12-22 23:56:18 +0900

    画像UP処理追加
    画像UP処理追加

--- index.js
+++ index.js
@@ -19,4 +19,20 @@ $(function () {
         sendText(msg);
         return false;
     });
+
+    $('#upfile').change(function(e){
+        var file = e.target.files[0];
+        var reader = new FileReader();
+        if(file.type.indexOf('image') < 0){
+          alert("画像ファイルを指定してください。");
+          return false;
+        }
+        reader.onload = (function(file){
+          return function(e){
+            $('#upimage').attr('src', e.target.result);
+            $('#upimage').attr('title', file.name);
+          };
+        })(file);
+        reader.readAsDataURL(file);
+      });
 });

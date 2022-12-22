f9afe32f4db2383a2943a37e64ae5f65a892ef6e
Yugo@Lance <takeshita.yugo@lance-digital.com>
2022-12-22 23:37:32 +0900

    処理を修正
    処理を修正

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 $(function () {
-    // カレンダー
-    $(function () {
-        $('input[name="date"]').datepicker({
-            dateFormat: 'yy/mm/dd',
-        });
-    });
-
     // 送信
     $('form').submit(function () {
+        //氏名
         var name = $('input[name="name"]').val();
-        var date = $('input[name="date"]:checked').val();
+        //生年月日
+        var year = $('input[name="year"]').val();
+        var month = $('input[name="month"]').val();
+        var day = $('input[name="day"]').val();
+        //住んでいる地域
         var area = $('input[name="area"]').val();
+        //職業
         var work = $('input[name="work"]').val();
 
-        var msg = `氏名：${name}\n生年月日：${date}\nお住まいの地域：${area}\n職業：${work}`;
+        var msg = `氏名：${name}
+            \n生年月日：${date}年${month}月${day}日
+            \nお住まいの地域：${area}
+            \n職業：${work}`;
         sendText(msg);
-
         return false;
     });
 });
